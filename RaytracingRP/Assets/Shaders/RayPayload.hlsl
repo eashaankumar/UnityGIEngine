d7880c5f398399b8578284c7b2a67855ef08ceef
Eashaan Kumar <eashaankumar@users.noreply.github.com>
2024-12-07 22:03:25 -0500

    DIRECT DIFFUSE WORKS!

--- RaytracingRP/Assets/Shaders/RayPayload.hlsl
+++ RaytracingRP/Assets/Shaders/RayPayload.hlsl
@@ -1,11 +1,14 @@
 struct RayPayload
 {
     float3 primateNormal;
+    float3 bounceRayOrigin;
+    float3 bounceRayDir;
     float4 primateColor;
     float4 color;
     float4 worldPos;
     uint bounceIndex;
     uint rayType;
+    uint rngState;
 };
 
 struct RayPayloadShadow
